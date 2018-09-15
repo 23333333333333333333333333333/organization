52a3c8cf5c2374d70fce9ddff6e33a6eebc1eec8
htxiang <2228672279@qq.com>
2018-09-15 17:06:29 +0800

    init

---
+++ webapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name organizationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the organizationApp
+ */
+angular.module('organizationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
